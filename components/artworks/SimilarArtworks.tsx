cd6f5155ee99924148c819c550221e56e1ffcf6c
Veskata98 <vescko332@gmail.com>
2024-04-15 14:11:57 +0300

    Fixed similar artworks view

--- components/artworks/SimilarArtworks.tsx
+++ components/artworks/SimilarArtworks.tsx
@@ -4,6 +4,7 @@ import { ArtworkCard } from '@/components/artwork/ArtworkCard';
 import { getSimilarArtworks } from '@/actions/artworkActions';
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
+import { Artwork } from '@/types';
 
 type SimilarArtworksProps = {
     category: string;
@@ -11,7 +12,10 @@ type SimilarArtworksProps = {
 };
 
 export const SimilarArtworks = async ({ category, id }: SimilarArtworksProps) => {
-    const similarArtworks = await getSimilarArtworks(category, id);
+    const similarArtworks: Artwork[] = await getSimilarArtworks(category, id);
+
+    if (!similarArtworks.length) return null;
+
     return (
         <div className="mt-6">
             <h2 className="text-xl">Подобни картини</h2>
@@ -20,15 +24,17 @@ export const SimilarArtworks = async ({ category, id }: SimilarArtworksProps) =>
                     {similarArtworks.map((artwork) => (
                         <ArtworkCard key={artwork.id} artwork={artwork} />
                     ))}
-                    <div className="flex flex-col justify-center items-center w-60">
-                        <p>Вижте още подобни картини</p>
-                        <Link
-                            href={`/artworks/${category}`}
-                            className="flex text-zinc-700 underline items-center justify-center gap-1"
-                        >
-                            тук <ArrowRight className="w-4 h-4" />
-                        </Link>
-                    </div>
+                    {similarArtworks.length === 12 && (
+                        <div className="flex flex-col justify-center items-center w-60">
+                            <p>Вижте още подобни картини</p>
+                            <Link
+                                href={`/artworks/${category}`}
+                                className="flex text-zinc-700 underline items-center justify-center gap-1"
+                            >
+                                тук <ArrowRight className="w-4 h-4" />
+                            </Link>
+                        </div>
+                    )}
                 </div>
 
                 <ScrollBar orientation="horizontal" />
