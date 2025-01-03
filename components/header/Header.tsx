7adfaa337d139259389fda06d2b7390983adce88
Agonts <vescko332@gmail.com>
2025-01-03 09:44:23 +0200

    Changed header nav color

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -13,7 +13,7 @@ export const Header = () => {
   const isActive = (href: string) => path === href || (path.startsWith(href) && path[href.length] === '/');
 
   return (
-    <nav className="flex justify-between items-center p-4 bg-secondary-foreground dark:bg-primary text-white md:px-8 max-h-16">
+    <nav className="flex justify-between items-center p-4 bg-neutral-700 text-white md:px-8 max-h-16">
       <Link href="/">
         <h1 className="text-lg md:text-xl font-bold text-center">Petar Yordanov Art</h1>
       </Link>
